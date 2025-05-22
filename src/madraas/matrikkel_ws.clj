ce8c61348996fe6b5c94e84348a42587ff493be1
Sigmund Hansen <sigmund.hansen@mattilsynet.no>
2025-05-22 10:39:38 +0200

    Legg til navnerom for å jobbe med SOAP-tjenestene til Matrikkelen
    Tar med en nyttig funksjon for å konvertere hiccup til XML som trengs for å lage
    SOAP-forespørsler.

---
+++ src/madraas/matrikkel_ws.clj
@@ -0,0 +1,23 @@
+(ns madraas.matrikkel-ws
+  (:require
+   [clojure.data.xml :as xml]))
+
+(xml/alias-uri 'xsi "http://www.w3.org/2001/XMLSchema-instance"
+               'soapenv "http://schemas.xmlsoap.org/soap/envelope/"
+               'dom "http://matrikkel.statkart.no/matrikkelapi/wsapi/v1/domain"
+               'ned "http://matrikkel.statkart.no/matrikkelapi/wsapi/v1/service/nedlastning"
+               'store "http://matrikkel.statkart.no/matrikkelapi/wsapi/v1/service/store"
+)
+
+(defn ->xml [hiccupish]
+  (cond (vector? hiccupish)
+        (let [[tag & content] hiccupish
+              has-attrs? (map? (first content))
+              attrs (when has-attrs? (first content))
+              content (if has-attrs? (rest content) content)]
+          (xml/element tag attrs (->xml content)))
+
+        (seq? hiccupish)
+        (map ->xml hiccupish)
+
+        :else hiccupish))
