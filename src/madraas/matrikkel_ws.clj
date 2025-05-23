74c1490e7ab714f41de663d693223f8ba187b4f9
Sigmund Hansen <sigmund.hansen@mattilsynet.no>
2025-05-23 10:17:53 +0200

    Fjern unødvendig funksjon, data.xml har sexp-as-element

--- src/madraas/matrikkel_ws.clj
+++ src/madraas/matrikkel_ws.clj
@@ -9,19 +9,6 @@
                'store "http://matrikkel.statkart.no/matrikkelapi/wsapi/v1/service/store"
 )
 
-(defn ->xml [hiccupish]
-  (cond (vector? hiccupish)
-        (let [[tag & content] hiccupish
-              has-attrs? (map? (first content))
-              attrs (when has-attrs? (first content))
-              content (if has-attrs? (rest content) content)]
-          (xml/element tag attrs (->xml content)))
-
-        (seq? hiccupish)
-        (map ->xml hiccupish)
-
-        :else hiccupish))
-
 (defn matrikkel-context [service-ns koordinatsystem]
   [(keyword (if (keyword service-ns)
               (name service-ns)
