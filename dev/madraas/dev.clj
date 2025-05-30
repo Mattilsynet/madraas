6502391e42b8e946a0ce86013582963650ddf954
Sigmund Hansen <sigmund.hansen@mattilsynet.no>
2025-05-30 15:34:28 +0200

    Legg til litt dokumenterende kode fra dev

--- dev/madraas/dev.clj
+++ dev/madraas/dev.clj
@@ -1,1 +1,46 @@
-(ns madraas.dev)
+(ns madraas.dev
+  (:require
+   [clojure.data.xml :as xml]
+   [clojure.edn :as edn]
+   [confair.config :as config]
+   [madraas.matrikkel-ws :as matrikkel-ws]
+   [madraas.system :as system]
+   [madraas.xml-helpers :as xh])
+  (:import
+   (java.time Duration)))
+
+(xml/alias-uri 'xsi     "http://www.w3.org/2001/XMLSchema-instance"
+               'soapenv "http://schemas.xmlsoap.org/soap/envelope/"
+               'dom     "http://matrikkel.statkart.no/matrikkelapi/wsapi/v1/domain"
+               'adr     "http://matrikkel.statkart.no/matrikkelapi/wsapi/v1/domain/adresse"
+               'endring "http://matrikkel.statkart.no/matrikkelapi/wsapi/v1/service/endringslogg"
+               'ned     "http://matrikkel.statkart.no/matrikkelapi/wsapi/v1/service/nedlastning"
+               'store   "http://matrikkel.statkart.no/matrikkelapi/wsapi/v1/service/store")
+
+(comment
+  (def config
+    (-> (system/init-config {:path "./config/dev-defaults.edn"})
+        (config/verify-required-together
+         #{#{:matrikkel/url
+             :matrikkel/username
+             :matrikkel/password}})
+        (config/mask-config)))
+
+  (def nats-conn
+    (system/init-connection config (edn/read-string
+                                    {:readers {'time/duration Duration/parse}}
+                                    (slurp "./resources/nats.edn"))))
+
+  (def veger
+    (:body (matrikkel-ws/be-om-såpe
+            config
+            "NedlastningServiceWS"
+            (matrikkel-ws/find-ids-etter-id-request "Veg" 53))))
+
+  (xml/element-nss veger)
+
+  (map (juxt #(-> % (xh/xsi-type matrikkel-ws/uri->ns-alias) matrikkel-ws/id-type->domene-klasse)
+             #(xh/get-in-xml % [::dom/item ::dom/value]))
+       (xh/get-in-xml veger [::soapenv/Envelope ::soapenv/Body ::ned/findIdsEtterIdResponse ::ned/return]))
+
+  )
